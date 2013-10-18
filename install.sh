9431940492da91d78a1f0065008f0489dff73ab8
minhta <minhta@localhost.localdomain>
2013-10-18 09:59:29 +0700

    add installation file

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+# after clone this respository into .vim folder, we should run this code to do some basic installation
+
+
+# should we remove ~/.vimrc first
+if [ -f ~/.vimrc]; then 
+	mv ~/.vimrc ~/.vimrc.bk
+fi 
+ 
+ln -s ~/.vim/vimrc ~/.vimrc
